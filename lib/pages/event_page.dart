b5de12e30cbdd019d336ff212fef440d2026a1f6
jamesattoh <koss.attoh27@gmail.com>
2025-04-13 00:20:52 +0100

    creation of static ListView

--- lib/pages/event_page.dart
+++ lib/pages/event_page.dart
@@ -10,7 +10,34 @@ class EventPage extends StatelessWidget {
         title: Text("Planning du Salon"),
       ),
       body: Center(
-        child: Text("Prochainement disponible"),
+        child: ListView(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Image.asset("assets/images/damien.jpg"),
+                title: Text("Damein Cavaillès (17h30 à 18H)"),
+                subtitle: Text("git blame --no-offense"),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Image.asset("assets/images/defendintelligence.jpg"),
+                title: Text("Defend Intelligence (18h à 18h30)"),
+                subtitle: Text("À la découverte de l'IA générative"),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Image.asset("assets/images/lior.jpg"),
+                title: Text("Lior Chamla (13h à 13h30)"),
+                subtitle: Text("Le code Legacy"),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
