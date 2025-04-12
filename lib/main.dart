815bc43a6974d0e698c0013d2902657b3a843547
jamesattoh <koss.attoh27@gmail.com>
2025-04-12 22:57:24 +0100

    creation of a redirect button

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:dolgen/pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,36 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) { // build assemble les elements mis à l'interieur de l'app
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold( // on definit la page d'accueil avec home, Scaffold est une pre - construction
-        appBar: AppBar(
-          title: Text("Dolgen 2025"),
-          backgroundColor: Colors.lightGreen,
-        ),
-        body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  "assets/images/logo.svg",
-                  colorFilter: ColorFilter.mode(Colors.lightGreen, BlendMode.srcIn),
-                  //color: Colors.lightGreen, cette utilisation est dépréciée
-                ),
-                Text("Dolgen 2025",
-                  style: TextStyle(
-                    fontSize: 42,
-                    fontFamily: 'Poppins'
-                  ),
-                ),
-                Text("Salon virtuel de l'informatique du 27 au 29 octobre 2025",
-                  style: TextStyle(
-                    fontSize: 24
-                  ),
-                  textAlign: TextAlign.center,
-                )
-              ],
-            )
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
