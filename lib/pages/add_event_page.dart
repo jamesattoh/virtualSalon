347fe0cd38cbee70c5cb89ffcb631de84946792f
jamesattoh <koss.attoh27@gmail.com>
2025-04-16 01:16:27 +0100

    retrieving field contents with controllers

--- lib/pages/add_event_page.dart
+++ lib/pages/add_event_page.dart
@@ -10,6 +10,19 @@ class AddEventPage extends StatefulWidget {
 class _AddEventPageState extends State<AddEventPage> {
   final _formKey = GlobalKey<FormState>(); // garder le statut (grace à FormState)  du formulaire avec toutes ses informations
 
+  // creation de controleurs : associent chaque champ a une mini-base qui, lorsqu'on écrit dans le champ, contiendra la valeur modifiée automatiquement
+  final confNameController = TextEditingController();
+  final speakerNameController = TextEditingController();
+
+  // quand on cree de la memoire il faut aussi penser a la liberer
+  @override
+  void dispose() {
+    super.dispose();
+
+    confNameController.dispose();
+    speakerNameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -32,6 +45,7 @@ class _AddEventPageState extends State<AddEventPage> {
                   }
                   return null;
                 },
+                controller: confNameController, // affectation du controleur au champ correspondant
               ),
             ),
             Container(
@@ -42,12 +56,13 @@ class _AddEventPageState extends State<AddEventPage> {
                     hintText: 'Entrez le nom du speaker',
                     border: OutlineInputBorder()
                 ),
-                validator: (value){ // il faut que ce validator s'enclenche (grace à un systeme de globalkey)
+                validator: (value){ // il faut que ce validator s'enclenche (grace à un systeme de globalkey) defini plus haut
                   if (value == null || value.isEmpty){
                     return "Tu dois entrer quelque chose";
                   }
                   return null;
                 },
+                controller: speakerNameController, // une sorte de lien entre la boite qui memorise le contenu et le champ sur lequel on va ecrire
               ),
             ),
             //SizedBox(height: 15),
@@ -57,11 +72,16 @@ class _AddEventPageState extends State<AddEventPage> {
               child: ElevatedButton(
                   onPressed: (){
                     if(_formKey.currentState!.validate()){
+                      final confName = confNameController.text; // on recupere le contenu des champs
+                      final speakerName = speakerNameController.text;
+
                       ScaffoldMessenger.of(context).showSnackBar(
                         const SnackBar(content: Text('Envoi en cours…'))
                       );
+                      FocusScope.of(context).requestFocus(FocusNode()); // lorsqu'on clique sur le bouton Envoyer, cette ligne de code ferme le clavier
+                      
+                      print(" Ajout de la conf $confName par le speaker $speakerName");
                     }
-                    FocusScope.of(context).requestFocus(FocusNode()); // lorsqu'on clique sur le bouton Envoyer, cette ligne de code ferme le clavier
                   },
                   style: ButtonStyle(
                     backgroundColor: WidgetStatePropertyAll(Colors.lightGreen)
