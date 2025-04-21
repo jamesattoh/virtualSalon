1a7774ce15fa2cff974edd8b674938ef6630c5f9
jamesattoh <koss.attoh27@gmail.com>
2025-04-21 23:40:00 +0100

    well displaying database data in the app

--- lib/pages/event_page.dart
+++ lib/pages/event_page.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class EventPage extends StatefulWidget {
   const EventPage({super.key});
@@ -9,47 +11,49 @@ class EventPage extends StatefulWidget {
 
 class _EventPageState extends State<EventPage> {
 
-  final events = [
-    {
-      "speaker" : "Damien Cavaillès",
-      "date" : "17h30 à 18h",
-      "subject" : "git blame --no-offense",
-      "avatar" : "damien"
-    },
-    {
-      "speaker" : "Defend Intelligence",
-      "date" : "18h à 18h30",
-      "subject" : "À la découverte de l'IA générative",
-      "avatar" : "defendintelligence"
-    },
-    {
-      "speaker" : "Lior Chamla",
-      "date" : "13h30 à 14h",
-      "subject" : "Le Low-code",
-      "avatar" : "lior"
-    },
-  ];
 
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: ListView.builder(
-          itemCount: events.length,
-          itemBuilder: (context, index) { //dans le builder de chaque element on va récupérer le contexte et l'index
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('Events').orderBy('date').snapshots(), // recuperation du flux de la collection
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) { // pour construire la vue
+            if (snapshot.connectionState == ConnectionState.waiting){
+              return CircularProgressIndicator();
+            }
 
-            final event = events[index];
-            final avatar = event['avatar'];
-            final speaker = event['speaker'];
-            final subject = event['subject'];
-            final date = event['date'];
+            if (!snapshot.hasData) {
+              return Text("Aucune conférence");
+            }
 
-            return Card(
-              child: ListTile(
-                leading: Image.asset('assets/images/$avatar.jpg'),
-                title: Text('$speaker ($date)'),
-                subtitle: Text('$subject'),
-                trailing: Icon(Icons.info_outline),
-              ),
+            // si on arrive ici, c'est que la requete a abouti eet qu'il y a des donnees dans la réponse
+            List<dynamic> events = [];
+
+            // snapshot.data!.docs.forEach( (element) { events.add(element); });
+            for (var element in snapshot.data!.docs) {
+              events.add(element);
+            }
+
+            return ListView.builder(
+              itemCount: events.length,
+              itemBuilder: (context, index) { //dans le builder de chaque element on va récupérer le contexte et l'index
+
+                final event = events[index];
+                final avatar = event['avatar'].toString().toLowerCase();
+                final speaker = event['speaker'];
+                final subject = event['subject'];
+                final Timestamp timestamp = event['date'];
+                final String date = DateFormat.yMd().add_jm().format(timestamp.toDate());
+
+                return Card(
+                  child: ListTile(
+                    leading: Image.asset('assets/images/$avatar.jpg'),
+                    title: Text('$speaker ($date)'),
+                    subtitle: Text('$subject'),
+                    trailing: Icon(Icons.info_outline),
+                  ),
+                );
+              },
             );
           },
         )
